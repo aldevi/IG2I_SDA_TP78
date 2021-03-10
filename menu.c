4cee8e994246b5ad856e0e6e10e8e1adb8a8f917
Teo-treny <treny.teojgh@gmail.com>
2021-03-10 14:07:30 +0100

    base livres cool + modif menu.c

--- menu.c
+++ menu.c
@@ -16,8 +16,9 @@ int searchBook(T_Bibliotheque *biblio) {
     for (int i = 0; i < biblio->nbLivres; i++) {
         if (strcmp(title, biblio->etagere[i].titre) == 0) {
             books_found++;
-            printf("\nLivre %d :\n", books_found);
+            printf("\n------------------ LIVRE %d ------------------\n", books_found);
             afficherLivre(&biblio->etagere[i]);
+            printf("---------------------------------------------");
         }
     }
     if (books_found > 0) {
